991f20380dd304b073610670ccc25f11412efbf1
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-10-25 19:30:07 +0200

    up date localhost

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true,
+  })
+);
 app.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
